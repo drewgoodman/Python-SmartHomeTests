354594698f3f409ecf2b4a351c04e625d1876d78
rdogm13 <drg6525@yahoo.com>
2021-03-06 17:02:54 -0800

    fixed syntax

--- control_smartplug.py
+++ control_smartplug.py
@@ -2,15 +2,18 @@ import asyncio
 from kasa import SmartPlug
 
 async def main():
-    plug = SmartPlug("10.0.0.32")
-    await plug.update()
-    print(plug.alias + " was located")
-    if plug.is_on:
-        await plug.turn_off()
-        print(plug.alias + " was shut off")
-    else:
-        await plug.turn_on()
-        print(plug.alias + " was turned on")
+    try:
+        plug = SmartPlug("10.0.0.32")
+        await plug.update()
+        print(f"{plug.alias} was located")
+        if plug.is_on:
+            await plug.turn_off()
+            print(f"{plug.alias}  was shut off")
+        else:
+            await plug.turn_on()
+            print(f"{plug.alias}  was turned on")
+    except:
+        print("Could not find a Smart Device at that address.")
 
 
 if __name__ == "__main__":
